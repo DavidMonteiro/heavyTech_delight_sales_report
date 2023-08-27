ae85fd636e3037089c13d8b3930218e8e82ded5e
DavidMonteiro <>
2023-08-27 08:26:35 +0000

    feat: python program created to generate csv files

---
+++ csv_generator.py
@@ -0,0 +1,8 @@
+# Define the possible options for nominal columns in lists
+finance_types = ['sale', 'rental']
+sellers = ['James Ford', 'Kate Austen', 'John Locke', 'Jack Shephard']
+business_sectors = ['Construction', 'Mining', 'Infrastructure', 'Demolition', 'Landscaping']
+product_makes = ['Caterpillar', 'Komatsu', 'Volvo', 'Haulage']
+product_types = ['Bulldozer', 'Excavator', 'Crane', 'Dump Truck', 'Backhoe Loader', 'Front End Loader', 'Concrete Pump Truck', 'Paver']
+product_states = ['new', 'refurbished']
+counties = ['Dublin', 'Cork', 'Galway', 'Limerick', 'Waterford', 'Kilkenny', 'Wexford', 'Meath', 'Kerry']
