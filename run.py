58a071a5116f1a01626886c1e885009b2138169a
DavidMonteiro <>
2023-08-27 09:00:44 +0000

    feat: Functions to analyse the dataframe are created

--- run.py
+++ run.py
@@ -9,4 +9,55 @@ try:
 except IOError as e:
      print('IO error: ' + e)
 
-print(sales)
+
+
+"""General functions for sales"""
+
+#function to count orders
+def count_orders(df):  
+    return df['Order_no'].count()
+
+
+#funcion to count hires
+def count_rentals(df):  
+    return (df['Finance_Type'] == 'rental').sum()
+
+
+#function to count capital purchase
+def count_sales(df):  
+    return (df['Finance_Type'] == 'sale').sum()
+
+
+#function to count products
+def count_products(df):  
+     return df['Product_Quantity'].sum()
+
+
+#function to count New Products
+def count_new_products(df):  
+    return (df.loc[df['Product_State'] == 'New']).Product_Quantity.sum()
+
+
+#Function to count S/Hand Products
+def count_refurbished_products(df):  
+    return (df.loc[df['Product_State'] == 'Refurbished']).Product_Quantity.sum()
+
+
+#Function to count no of unique companies
+def count_companies(df):  
+    return df['Company'].nunique()
+
+
+#Function to count no of unique counties/areas
+def count_counties(df):  
+    return df['County'].nunique()
+
+
+print(count_orders(sales))
+print(count_rentals(sales))
+print(count_sales(sales))
+print(count_products(sales))
+print(count_new_products(sales))
+print(count_refurbished_products(sales))
+print(count_companies(sales))
+print(count_counties(sales))
