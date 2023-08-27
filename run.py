a709b4e0f839eb5f19a08829ea4387c0128ff981
DavidMonteiro <>
2023-08-27 09:33:43 +0000

    feat: donut chart added as a function / feat: general table modified into a function

--- run.py
+++ run.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.io as pio
+import plotly.express as px
 
 #Creating Sale dataframes from csv file > 'heavyTech_delight_sales_2021.csv'
 try:
@@ -33,11 +34,11 @@ def count_products(df):
 
 """function to count New Products""" 
 def count_new_products(df): 
-    return (df.loc[df['Product_State'] == 'New']).Product_Quantity.sum()
+    return (df.loc[df['Product_State'] == 'new']).Product_Quantity.sum()
 
 """Function to count S/Hand Products"""
 def count_refurbished_products(df):  
-    return (df.loc[df['Product_State'] == 'Refurbished']).Product_Quantity.sum()
+    return (df.loc[df['Product_State'] == 'refurbished']).Product_Quantity.sum()
 
 """Function to count no of unique companies"""
 def count_companies(df):  
@@ -57,26 +58,35 @@ print(count_refurbished_products(sales))
 print(count_companies(sales))
 print(count_counties(sales))
 
-fig = go.Figure(data=[go.Table(header=dict(values=['Description:', 'Results']),
-                 cells=dict(values=[
-                     [
-                        "Total Orders",
-                        "Rentals",
-                        "Sales",
-                        "Total Products",
-                        "New Products",
-                        "Refurbished Products"
-                    ], 
-                     [
-                        count_orders(sales),
-                        count_rentals(sales),
-                        count_sales(sales),
-                        count_products(sales),
-                        count_new_products(sales),
-                        count_refurbished_products(sales)]]))
-                     ])
-fig.update_layout(width=500)
-fig.show()
-html = pio.to_html(fig, include_plotlyjs='cdn')
-fig.write_html("test.html")
-fig.write_html("test.html")
+def general_data_table(df):
+    """Function to generate table with sales data into an html file"""
+    fig = go.Figure(data=[go.Table(header=dict(values=['Description:', 'Results']),
+                    cells=dict(values=[
+                        [
+                            "Total Orders",
+                            "Rentals",
+                            "Sales",
+                            "Total Products",
+                            "New Products",
+                            "Refurbished Products"
+                        ], 
+                        [
+                            count_orders(df),
+                            count_rentals(df),
+                            count_sales(df),
+                            count_products(df),
+                            count_new_products(df),
+                            count_refurbished_products(df)]]))
+                        ])
+    fig.update_layout(width=500)
+    fig.write_html("test.html")
+
+def general_data_donutChart(df):
+    """Function to generate pie chart with sales data into an html file """
+    data = df.groupby('Finance_Type')['Order_no'].count().reset_index()
+    fig = px.pie(data, values='Order_no', names='Finance_Type', color = 'Finance_Type',
+                    color_discrete_map = {'Sales': '#003f5c', 'Rentals': '#ffa600'})
+    fig.update_traces(hole=.4, textinfo='value + percent',hovertemplate = "%{label}<br>Total Orders: %{value} </br>Percentage : %{percent}")
+    fig.write_html("test.html")
+
+general_data_donutChart(sales)
