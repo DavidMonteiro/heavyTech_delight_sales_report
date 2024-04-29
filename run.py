41cec4f791524d6ec1548e9599cd2cb5ba9c8e6f
David Monteiro <david.frtd.monteiro@gmail.com>
2024-04-29 15:17:05 +0000

    feat: Added functions for generating HTML report and integrated into run.py

--- run.py
+++ run.py
@@ -79,7 +79,7 @@ def general_data_table(df):
                             count_refurbished_products(df)]]))
                         ])
     fig.update_layout(width=500)
-    fig.write_html("test.html")
+    return fig.to_html(full_html=True, include_plotlyjs='cdn')
 
 def general_data_donutChart(df):
     """Function to generate pie chart with sales data into an html file """
@@ -87,6 +87,53 @@ def general_data_donutChart(df):
     fig = px.pie(data, values='Order_no', names='Finance_Type', color = 'Finance_Type',
                     color_discrete_map = {'Sales': '#003f5c', 'Rentals': '#ffa600'})
     fig.update_traces(hole=.4, textinfo='value + percent',hovertemplate = "%{label}<br>Total Orders: %{value} </br>Percentage : %{percent}")
-    fig.write_html("test.html")
+    return fig.to_html(full_html=True, include_plotlyjs='cdn')
 
-general_data_donutChart(sales)
+
+
+def start_html_report():
+    """Function to start a single HTML page"""
+    html_content0 = "<html>\n"
+    html_content0 += "<head>\n"
+    html_content0 += "<title>Sales Data</title>\n"
+    html_content0 += "</head>\n"
+    html_content0 += "<body>\n"
+
+    return html_content0
+
+def push_to_html_report(html_content0, html_block0):
+    """Function to add a single HTML section to the HTML page"""
+    html_content0 += html_block0
+    html_content0 += "\n"
+    return html_content0
+
+def close_html_report(html_content0):
+    """Function to close the HTML page"""
+    html_content0 += "</body>\n"
+    html_content0 += "</html>\n"
+    return html_content0
+
+def generate_html_report(html_filename0, html_block0):
+    """Function generate and save a single HTML page"""
+    file = open(html_filename0, "w") 
+    file.write(html_block0)
+    file.close()
+
+
+#Start the html page as a string
+html_content = start_html_report()
+
+# Generate the table and the donut chart
+table_fig = general_data_table(sales)
+donut_chart_fig = general_data_donutChart(sales)
+
+#Push the table and the chart into the html string
+html_content = push_to_html_report(html_content, table_fig)
+html_content = push_to_html_report(html_content, donut_chart_fig)
+
+
+#Close the html page </body? & </html>
+html_content = close_html_report(html_content)
+
+#Generate html report in html format
+generate_html_report("sales_report.html", html_content)
