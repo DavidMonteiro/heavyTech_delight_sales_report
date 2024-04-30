b66e9cd2ea4a6089bd6135175386c74c444bad68
David Monteiro <david.frtd.monteiro@gmail.com>
2024-04-30 07:53:37 +0000

    update: changed main program to have an interactive UI

--- run.py
+++ run.py
@@ -10,17 +10,18 @@ import general_report as gen_report
 import order_report as ord_report
 import product_report as prd_report
 import report_generator as generator
+import csv_generator as myCSV
 
-##ETL section - extract csv into dataframe and add two columns after making some adjustments
-sales = io.read_csv('heavyTech_delight_sales_2021.csv')
 
-# Change the format of column Date to type datetime
-sales["Date"] = pd.to_datetime(sales["Date"], format="%Y-%m-%d")
-
-# Create a new column for month and quarter
-sales['Month'] = sales['Date'].dt.month_name()
-sales['Quarter'] = sales['Date'].dt.quarter
-
+#def upload_sales_data():
+#   #User gets to upload his own sales data
+#   file_path = input("Please enter the path to the CSV file you want to upload: \n")
+#
+#   # Check if the file exists
+#   if os.path.exists(file_path):
+#       generate_report(file_path)
+#   else:
+#       print("File not found!")
 
 
 def start_html_report():
@@ -46,22 +47,48 @@ def close_html_report(html_content0):
     return html_content0
 
 
-#Start the html page as a string
-html_content = start_html_report()
+def generate_report(filename):
 
-## Generate the table and the donut chart
-#html_content = gen_report.general_Analysis(sales, html_content)
-#html_content = ord_report.order_Analysis(sales, html_content)
-#html_content = prd_report.product_Analysis(sales, html_content)
-html_content += generator.completeAnalysis(sales)
+    ##ETL section - extract csv into dataframe and add two columns after making some adjustments
+    sales = io.read_csv('heavyTech_delight_sales_2021.csv')
+
+    # Change the format of column Date to type datetime
+    sales["Date"] = pd.to_datetime(sales["Date"], format="%Y-%m-%d")
+
+    # Create a new column for month and quarter
+    sales['Month'] = sales['Date'].dt.month_name()
+    sales['Quarter'] = sales['Date'].dt.quarter
+
+    #Start the html page as a string
+    html_content = start_html_report()
+
+    ## Generate the Analysis
+    html_content += generator.completeAnalysis(sales)
 
 
-#Close the html page </body? & </html>
-html_content = close_html_report(html_content)
+    #Close the html page </body? & </html>
+    html_content = close_html_report(html_content)
 
 
-#Generate html report in html format
-io.generate_html_report("sales_report.html", html_content)
+    #Generate html report in html format
+    io.generate_html_report("sales_report.html", html_content)
 
 
+def run_Main():
+    print("Hi! This is the Report Generator for Sales of Heavy Machinery!")
+    choice = input("Do you want to use a mock CSV (enter '1') or upload your own CSV (enter '2')? ")
+
+    if choice.lower() == '1':
+        myCSV.generate_sales_data(200)
+        generate_report('heavyTech_delight_sales_2021.csv')
+        
+    elif choice.lower() == '2':
+        #upload_sales_data()
+        print("Oops.. This is still in construction")
+    else:
+        #I am calling the function itself making it recursive
+        print("Invalid choice. Please enter '1' or '2'.")
+        run_Main()
+
+run_Main()
 
